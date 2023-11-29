fb385441f20157fb37bc57eaec8142b86d2d4e4a
Berk GOZEK <144159613+BerkGozek@users.noreply.github.com>
2023-11-30 00:13:16 +0300

    ini komm susser todd
    everything is done
    flower is missing tho
    kinda defeats the whole point of the project (maybe)

--- FlowerOfTheHanged/ContentView.swift
+++ FlowerOfTheHanged/ContentView.swift
@@ -8,14 +8,118 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var onScreen = "-----";
+    @State var answer = "-----";
+    @State var ansList = ["sedat","allah","cryin","swift","death","pluto","hades","shiva"];
+    @State var ansReadOnly = "-----";
+    
+    @State var wordsGuessed = 0;
+    @State var wordsLeft : Int = 0;
+    
+    @State var livesUsed    =   0   ;
+    @State var livesLeft    =   6   ;
+    
+    @State var ans          =   ""  ;
+    
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            HStack{
+                
+                Text("Words Left:")
+                    .padding(.leading)
+                Text(String(wordsLeft))
+                Spacer()
+                Text("Words Found:")
+                Text(String(wordsGuessed))
+                    .padding(.trailing)
+                
+            }
+            HStack{
+                
+                Text("Lives Left:")
+                    .padding(.leading)
+                Text(String(livesLeft))
+                Spacer()
+                Text("Lives Spent:")
+                
+                Text(String(livesUsed))
+                    .padding(.trailing)
+                
+            }
+            .padding(.trailing)
+            Rectangle()
+                .frame(width: 200, height: 300, alignment: .center)
+            HStack{
+                TextField("", text: $ans)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(width: 30.0, height: 50.0)
+                
+                Text(onScreen)
+                    .font(.title)
+            }
+            Button("Submit Letter"){
+                isInWord(skibidi: ans)
+                if (foundItYet()){
+                    newWord()
+                }
+            }.buttonBorderShape(.capsule)
+                .buttonStyle(.bordered)
+            Spacer()
+            
         }
-        .padding()
+        .onAppear(){
+            newWord()
+            wordsLeft = ansList.count
+        }
+        
+    }
+    
+    func newWord(){
+        onScreen  = "-----";
+        answer = ansList.randomElement() ?? "hello"
+        ansReadOnly = answer
+        
+    }
+    
+    func isInWord(skibidi: String) {
+        var bet = skibidi.lowercased()
+        if (bet != "") {
+            if answer.contains(bet) {
+                var onScreenArray = Array(onScreen)
+                var answerArray = Array(answer)
+                
+                for (index, char) in answer.enumerated() {
+                    if String(char) == bet {
+                        onScreenArray[index] = bet.first!
+                        answerArray[index] = "-"
+                    }
+                    
+                }
+                
+                
+                onScreen = String(onScreenArray)
+                answer = String(answerArray)
+            }
+            else{
+                livesUsed = livesUsed+1;
+                livesLeft = livesLeft-1;
+            }
+        }
+        ans = ""
+        
+    }
+    
+    func foundItYet()->Bool{
+        if(ansReadOnly == onScreen){
+            wordsGuessed = wordsGuessed + 1
+            return true
+        }
+        else
+        {
+            return false
+        }
     }
 }
 
