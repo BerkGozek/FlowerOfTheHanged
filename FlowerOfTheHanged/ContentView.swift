19bf96899309e22cc3d91371e30e69b729986014
Berk GOZEK <144159613+BerkGozek@users.noreply.github.com>
2023-11-29 20:26:16 +0300

    Initial Commit

---
+++ FlowerOfTheHanged/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlowerOfTheHanged
+//
+//  Created by Berk Gozek on 29/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlowerOfTheHanged/FlowerOfTheHangedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerOfTheHangedApp.swift
+//  FlowerOfTheHanged
+//
+//  Created by Berk Gozek on 29/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlowerOfTheHangedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
